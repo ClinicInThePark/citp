05ae640ebd8fdf03b359eacc59583edcd633e470
Ka Lun Lee <kalun.lee1@gmail.com>
2014-11-02 19:11:05 -0800

    passport field color change + msg box when clicked
    When a user clicks a passport box, the box color is changed and a
    message box is displayed. Custom message is not yet available.

--- app/assets/javascripts/passport_display.js
+++ app/assets/javascripts/passport_display.js
@@ -1,10 +1,30 @@
 //automatically loads Jquery code when the DOM finishes loading. 
 $(document).ready(function(){ 
-	$('.passport_field').on('click', function(e){
-		$(this).addClass('checked')
-		$('options').css("display","block")
-
+	
+	$('.passport_field').click(function(e) {
+		if ($(this).hasClass('checked')){
+			$(this).removeClass('checked')
+		}else{
+			$(this).addClass('checked')
+		}
+		$('.options').css({
+			'left': e.clientX + 'px',
+			'top': e.clientY + 'px',
+			'display': 'block'
+			})
+		$('.options').fadeIn('fast');
 	});
+
+	$( document ).mouseup( function ( event ) {
+      var $container = $( '.main-image' );
+      // if the target of the click isn't the container...
+      if ( !$container.is( event.target ) &&
+          // ... nor a descendant of the container
+          $container.has( event.target ).length === 0 ) { 
+        $( '.options' ).fadeOut( 'fast' );
+        
+      }
+    });
 
 	
 
